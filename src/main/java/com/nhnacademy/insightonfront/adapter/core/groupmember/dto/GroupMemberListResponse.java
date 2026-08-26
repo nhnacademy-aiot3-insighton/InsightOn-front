6e35b0cb1228af1d3c7578b39c509aac50ab1f50
naeun912 <st243235@gmail.com>
2026-08-26 16:28:57 +0900

    fix: 잘못된 경로 수정 및 dto 수정

--- src/main/java/com/nhnacademy/insightonfront/adapter/core/groupmember/dto/GroupMemberListResponse.java
+++ src/main/java/com/nhnacademy/insightonfront/adapter/core/groupmember/dto/GroupMemberListResponse.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.insightonfront.adapter.core.groupmember.dto;
 
 public record GroupMemberListResponse(
+        Long groupMemberId,
         Long userId,
         GroupRole groupRole
 ) {
